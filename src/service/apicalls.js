b69a91168030890edfd7591f2024924a29f388ea
anujChoudhary-1712 <153001086+anujChoudhary-1712@users.noreply.github.com>
2024-11-05 15:59:34 +0530

    Update apicalls.js

--- src/service/apicalls.js
+++ src/service/apicalls.js
@@ -1,2 +1,2 @@
 // export const apiString = "https://api.indiestori.com/api";
-export const apiString = "http://193.203.163.161:8000";
+export const apiString = "https://indiebackend.adultssolutions.com";
